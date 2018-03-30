2cea6d2337999c25a054887143fdd31ecec00071
Warren-Partridge <maps@perdix.org>
2018-03-30 13:39:31 -0400

    Make popups closable with escape key

--- js/popups.js
+++ js/popups.js
@@ -1,16 +1,29 @@
+var targetedPopupClass; // Declare globally, so the escape key function knows which popup to close
+
 $(function() {
-//----- OPEN
-
+    // Open popup
     $('[data-popup-open]').on('click', function(e)  {
-        var targeted_popup_class = jQuery(this).attr('data-popup-open');
-        $('[data-popup="' + targeted_popup_class + '"]').fadeIn(200);
+        targetedPopupClass = jQuery(this).attr('data-popup-open');
+        $('[data-popup="' + targetedPopupClass + '"]').fadeIn(200);
         e.preventDefault();
     });
 
-//----- CLOSE
+    // Close popup
     $('[data-popup-close]').on('click', function(e)  {
-        var targeted_popup_class = jQuery(this).attr('data-popup-close');
-        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(200);
+        // targeted_popup_class = jQuery(this).attr('data-popup-close');
+
+        // I believe the above code is unnecessary because targeted_popup_class is already set from opening the popup,
+        // and we should never have 2 popups open at once
+
+        $('[data-popup="' + targetedPopupClass + '"]').fadeOut(200);
         e.preventDefault();
     });
+});
+
+
+// Close the popup if the escape key is pressed
+$(document).keydown(function(e) {
+    if (e.keyCode === 27) {
+        $('[data-popup="' + targetedPopupClass + '"]').fadeOut(200);
+    }
 });
